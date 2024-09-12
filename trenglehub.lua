5fabf613de244cf70050d286a1e8f1cf6019de33
External Bypassed <neerurani740@gmail.com>
2024-09-12 18:18:45 +0100

    Updated

--- trenglehub.lua
+++ trenglehub.lua
@@ -1624,6 +1624,83 @@ local universalsearchtextbox = universalsearch:NewTextBox("Search", "Seafch for 
                         "Status: You have failed to execute this script before")
                     end
                 end)
+
+    -- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local ImageButton = Instance.new("ImageButton")
+local UICorner = Instance.new("UICorner")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+ImageButton.Parent = ScreenGui
+ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderSizePixel = 0
+ImageButton.Position = UDim2.new(0.102370687, 0, 0.176282048, 0)
+ImageButton.Size = UDim2.new(0, 54, 0, 54)
+ImageButton.Image = "rbxassetid://90451194349182"
+
+UICorner.Parent = ImageButton
+
+-- Scripts:
+
+local function PCTYAP_fake_script() -- ImageButton.LocalScript 
+	local script = Instance.new('LocalScript', ImageButton)
+
+	local UIS = game:GetService('UserInputService')
+	local frame = script.Parent
+	local dragToggle = nil
+	local dragSpeed = 0.25
+	local dragStart = nil
+	local startPos = nil
+	
+	local function updateInput(input)
+		local delta = input.Position - dragStart
+		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+	end
+	
+	frame.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+			dragToggle = true
+			dragStart = input.Position
+			startPos = frame.Position
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragToggle = false
+				end
+			end)
+		end
+	end)
+	
+	UIS.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			if dragToggle then
+				updateInput(input)
+			end
+		end
+	end)
+end
+coroutine.wrap(PCTYAP_fake_script)()
+local function TINARWG_fake_script() -- ImageButton.LocalScript 
+	local script = Instance.new('LocalScript', ImageButton)
+
+	local TB =  script.Parent
+	
+	TB.MouseButton1Down:Connect(function()
+		Library:ToggleUI()
+	end)
+end
+coroutine.wrap(TINARWG_fake_script)()
+
                 getgenv().QuerySection:NewButton("Copy Origin link of Script", v.origin, function()
                     setclipboard(v.origin)
                     bigGreenItalicText("Copied Origin link of Script to clipboard!")
